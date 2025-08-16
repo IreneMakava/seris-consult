0a1b1181f0ebb99fb09bc1f215d3f02a9c56d73e
IreneMakava <irenemakava@gmail.com>
2025-08-16 14:37:15 +0300

    vite config changes

--- seris-consult/vite.config.js
+++ seris-consult/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // (if using React)
-
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(), // (if using React)
-  ],
-  root:'.',
+  plugins: [react()],
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html' // Explicit entry point
+      }
+    }
+  },
+  server: {
+    open: true // Optional: opens browser on dev server start
+  }
 });
